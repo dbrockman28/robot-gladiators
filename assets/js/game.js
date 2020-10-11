b8ee7ff946ecc048700a7b7fa9c79aaa64465d39
Rowdy Goat <dbrockman28@gmail.com>
2020-10-11 18:48:23 -0500

    refined fight prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,44 +17,47 @@ let enemyAttack = 12;
 
 let fight = function(enemyName) {
 
-    while(enemyHealth > 0) {
+    while(enemyHealth > 0 && playerHealth > 0) {
         let promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
-        //Subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth variable.
-        if (promptFight === "fight" || promptFight === "FIGHT") {
-            enemyHealth = enemyHealth - playerAttack;
-            //Log a resulting message to the console so we know that it worked.
-            console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
-            //Check enemy's health.
-            if (enemyHealth <= 0) {
-                window.alert(enemyName + " has died!");
-            } else {
-                window.alert(enemyName + " has " + enemyHealth + " health left.");
-            }
-            //Subtract the value of enemyAttack from the value of playerHealth and use that result to update the value in the playerHealth variable.
-            playerHealth = playerHealth - enemyAttack;
-            //Log a resulting message to the console so we know that it worked.
-            console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
-            //Check player health.
-            if (playerHealth <= 0) {
-                window.alert(playerName + " has died!")
-            } else {
-                window.alert(playerName + " has " + playerHealth + " health reamaining.");
-            }
-        } else if (promptFight === "skip" || promptFight === "SKIP") {
+
+        if (promptFight === "skip" || promptFight === "SKIP") {
             //Confirm user wnats to skip.
             let confirmSkip = window.confirm("Are you sure you'd like to skip");
             //If yes, skip fight.
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip the fight. Goodbye!");
             //Subtract money from playerMoney for skipping.
-                playerMoney = playerMoney - 2;
-                console.log(playerMoney);
-            }
-            //If no, ask question again by running fight() again.
-        else {
-            fight();
+                playerMoney = playerMoney - 10;
+                console.log(playerName + "'s money", playerMoney);
+                break;
             }
         }
+        //Subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth variable.
+        enemyHealth = enemyHealth - playerAttack;
+        //Log a resulting message to the console so we know that it worked.
+        console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+        //Check enemy's health.
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died!");
+            //Award money for defeating enemy and exit loop
+            playerMoney = playerMoney + 20;
+            break;
+        } else {
+            window.alert(enemyName + " has " + enemyHealth + " health left.");
+        }
+        //Subtract the value of enemyAttack from the value of playerHealth and use that result to update the value in the playerHealth variable.
+        playerHealth = playerHealth - enemyAttack;
+        //Log a resulting message to the console so we know that it worked.
+        console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+        //Check player health.
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!")
+            break;
+        } else {
+            window.alert(playerName + " has " + playerHealth + " health reamaining.");
+        }
+        
     }
         
 };
